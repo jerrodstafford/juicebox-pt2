8bd1799a844a66e3b3f56cb5f667a56bf7eb8b27
Jerrod Stafford <jerrodstafford10@gmail.com>
2022-09-19 16:07:13 -0500

    Fixed incorrect placement of token variable declaration

--- api/users.js
+++ api/users.js
@@ -3,36 +3,43 @@ const usersRouter = express.Router();
 const { getAllUsers, getUserByUsername, createUser } = require('../db');
 
 const jwt = require('jsonwebtoken');
-const token = jwt.sign({ id: 1, username: "albert" }, process.env.JWT_SECRET, { expiresIn: '1week' });
 
+
+usersRouter.use('/', async(req, res) => {
+  const users = await getAllUsers();
+  
+  res.send({
+    users
+  });
+});
 
 usersRouter.post('/register', async (req, res, next) => {
   const { username, password, name, location } = req.body;
-
+  
   try {
     const _user = await getUserByUsername(username);
-
+    
     if (_user) {
       next({
         name: 'UserExistsError',
         message: 'A user by that username already exists'
       });
     }
-
+    
     const user = await createUser({
       username,
       password,
       name,
       location,
     });
-
+    
     const token = jwt.sign({ 
       id: user.id, 
       username
     }, process.env.JWT_SECRET, {
       expiresIn: '1w'
     });
-
+    
     res.send({ 
       message: "thank you for signing up",
       token 
@@ -44,19 +51,20 @@ usersRouter.post('/register', async (req, res, next) => {
 
 usersRouter.use('/login', async(req, res, next) => {
   const { username, password } = req.body;
-
+  
   if(!username || !password) {
     next({
       name: "MissingCredentialsError",
       message: "Please supply both a username and password"
     });
   }
-
-
+  
+  
   try {
     const user = await getUserByUsername(username);
-
+    
     if (user && user.password == password) {
+      const token = jwt.sign({ id: 1, username: "albert" }, process.env.JWT_SECRET, { expiresIn: '1week' });
       res.send({ 
         message: "You're logged in!",
         "token": token
@@ -74,12 +82,5 @@ usersRouter.use('/login', async(req, res, next) => {
 });
 
 
-usersRouter.use('/', async(req, res) => {
-    const users = await getAllUsers();
-
-    res.send({
-        users
-    });
-});
 
 module.exports = usersRouter;
