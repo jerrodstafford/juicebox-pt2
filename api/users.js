ed034cff5be2e8f11638ca48f83623618c21e7e3
Jerrod Stafford <jerrodstafford10@gmail.com>
2022-09-26 00:38:22 -0500

    last Minute Changes

--- api/users.js
+++ api/users.js
@@ -42,7 +42,7 @@ usersRouter.post('/register', async (req, res, next) => {
   } 
 });
 
-usersRouter.use('/login', async(req, res, next) => {
+usersRouter.post('/login', async(req, res, next) => {
   const { username, password } = req.body;
   
   if(!username || !password) {
